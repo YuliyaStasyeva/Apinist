ea00b4a0305faa24fbbc07301fe9296673a25399
YuliyaStasyeva <40565797+YuliyaStasyeva@users.noreply.github.com>
2019-01-09 12:07:15 +0300

    Update test_script.js
    fix

--- test_script.js
+++ test_script.js
@@ -1,18 +1,22 @@
-var yandexMainPage= require ('./po_main');
-var yandexLocation = require ('./po_location')
+const yandexMainPage= require ('./po_main');
+const yandexLocation = require ('./po_location')
 describe('match tab', function() {
 	  it('should match for London and Paris', async function() {
       await yandexMainPage.get();
       await yandexMainPage.getPageLocation();
       await yandexLocation.clearInput();
       await yandexLocation.setCity('Лондон');
+      await yandexLocation.waitForAutocopleteLondon();
       await yandexLocation.selectCity();
+      await yandexMainPage.waitForElse();
       await yandexMainPage.elseClick();
       await yandexMainPage.getMatchLinkLondon();
       await yandexMainPage.getPageLocation();
       await yandexLocation.clearInput();
       await yandexLocation.setCity('Париж');
+      await yandexLocation.waitForAutocopleteParis();
       await yandexLocation.selectCity();
+      await yandexMainPage.waitForElse();
       await yandexMainPage.elseClick();
       await yandexMainPage.getMatchLinkParis();
       expect(yandexMainPage.getMatchLinkLondon()).toEqual(yandexMainPage.getMatchLinkParis());
