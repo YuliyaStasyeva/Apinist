a644020d87c783493815e295e720e98139bf6fa1
YuliyaStasyeva <40565797+YuliyaStasyeva@users.noreply.github.com>
2019-01-10 14:17:49 +0300

    new

--- po_location.js
+++ po_location.js
@@ -1,22 +1,18 @@
-let cityInput=element(by.css('#city__front-input'));
-let selectInput=element(by.css('ul>li:nth-child(1).b-autocomplete-item'));
+let variables={
+    cityInput:element(by.css('#city__front-input')),
+    selectInput:element(by.css('li:nth-child(1).b-autocomplete-item')),
+   };
+let waitAutocomplete=require ('./waiters');
 class YandexLocation {
 	async clearInput() {
-		  await cityInput.clear();
+		  await variables.cityInput.clear();
 	};
 	async setCity(city) {
-          await cityInput.sendKeys(city);
+          await variables.cityInput.sendKeys(city);
 	};
-    async selectCity() {
-          await selectInput.click();
+    async selectCity(country) {
+          await waitAutocomplete.waitForPresentTextInElement(variables.selectInput,country);
+          await variables.selectInput.click();
     };
-    async waitForAutocopleteParis() {
-          let EC=protractor.ExpectedConditions;
-          await browser.wait(EC.textToBePresentInElement(selectInput, "Париж"), 5000);
-	};
-    async waitForAutocopleteLondon() {
-          let EC=protractor.ExpectedConditions;
-          await browser.wait(EC.textToBePresentInElement(selectInput, "Лондон"), 5000);
     };
-};
 module.exports=new YandexLocation();
